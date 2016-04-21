1b1ae873f22413ccdf0bcdf7f2ab41dbc4a3f037
Hirak <hiraksarkar.cs@gmail.com>
2016-04-21 05:35:35 -0400

    time benchmarking

--- kseq_test.cpp
+++ kseq_test.cpp
@@ -5,9 +5,10 @@
 #include <algorithm>
 #include <unordered_map>
 #include <fstream>
+#include <sys/time.h>
+
 #include "kseq.h"
 KSEQ_INIT(gzFile, gzread)
-
 struct read_record{
     std::string name;
     std::string seq;
@@ -40,12 +41,21 @@ int main(int argc, char *argv[])
     std::ifstream ifs;
     ifs.open(argv[2],std::ifstream::in) ;
     int rank = 0;
+    std::cout << "Start reading read ids from "<<argv[2] << "\n";
+
+
     for(std::string line; std::getline(ifs, line) ;){
         nameToRank[line] = rank ;
+        if (rank%10000 == 0){
+            std::cout << rank+1 << " reads processed \r" << std::flush ;
+        }
         rank++;
     }
 
+
+    std::cout << "\n Start reading read from fastq "<<argv[1] << "\n";
 	seq = kseq_init(fp);
+    int numReads = 0;
 	while ((l = kseq_read(seq)) >= 0) {
 
 
@@ -59,7 +69,15 @@ int main(int argc, char *argv[])
         std::string quali(seq->qual.s);
         read_record v(name,sequ,quali);
         allreads.push_back(v);
+        if (numReads%10000 == 0){
+            std::cout << numReads+1 << " reads processed \r" << std::flush ;
+        }
+        numReads++;
 	}
+
+    std::cout << "\n Start sorting \n" ;
+    timespec startt,endt,result;
+    clock_gettime(CLOCK_REALTIME, &startt);
     std::sort(allreads.begin(), allreads.end(), [&nameToRank](const read_record& r1,
                 const read_record& r2) -> bool {
                 //std::cout << r1.name << "\n";
@@ -67,18 +85,28 @@ int main(int argc, char *argv[])
                 //std::cout<< nameToRank[r1.name] << nameToRank[r2.name] << "\n" ;
                 return nameToRank[r1.name] < nameToRank[r2.name]; }) ;
 
+    clock_gettime(CLOCK_REALTIME, &endt);
+    //inttimeval_subtract(&result,&startt,&endt);
+    std::cout << "\n Sorting done, took "<< (startt.tv_sec - endt.tv_sec) << " ns to finish \n";
+
     std::ofstream ofs;
     ofs.open(argv[3], std::ofstream::out) ;
 
+    std::cout << "\n Start writing to shuffled fastq "<<argv[3] << "\n";
 
+    numReads = 0;
     for(auto &readid: allreads){
         ofs << "@" << readid.name << "\n" ;
         ofs << readid.seq<< "\n" ;
         ofs << readid.comment << "\n" ;
         ofs << readid.quality << "\n" ;
+        if (numReads%10000 == 0){
+            std::cout << numReads+1 << " reads written \r" << std::flush ;
+        }
+        numReads++;
     }
 
-	printf("return value: %d\n", l);
+	//printf("return value: %d\n", l);
 	kseq_destroy(seq);
     ifs.close() ;
     ofs.close() ;
