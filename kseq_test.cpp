ee2b76599cb45992f32778c4928df467d199e9b6
Hirak <hiraksarkar.cs@gmail.com>
2016-04-21 00:37:33 -0400

    shuffling with c++ code uses kseq

---
+++ kseq_test.cpp
@@ -0,0 +1,87 @@
+#include <zlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <fstream>
+#include "kseq.h"
+KSEQ_INIT(gzFile, gzread)
+
+struct read_record{
+    std::string name;
+    std::string seq;
+    std::string quality;
+    std::string comment = "+";
+    public:
+    read_record(std::string &name, std::string &seq, std::string &quality):
+        name(name), seq(seq), quality(quality) {
+
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	gzFile fp;
+	kseq_t *seq;
+	int l;
+
+    //create vector of all read sequence
+    //std::vector<kseq_t *> allreads ;
+    std::vector<read_record > allreads ;
+    //std::vector<std::string > readids ;
+    std::unordered_map<std::string,int > nameToRank ;
+
+	if (argc == 1) {
+		fprintf(stderr, "Usage: %s <in.seq>\n", argv[0]);
+		return 1;
+	}
+	fp = gzopen(argv[1], "r");
+    std::ifstream ifs;
+    ifs.open(argv[2],std::ifstream::in) ;
+    int rank = 0;
+    for(std::string line; std::getline(ifs, line) ;){
+        nameToRank[line] = rank ;
+        rank++;
+    }
+
+	seq = kseq_init(fp);
+	while ((l = kseq_read(seq)) >= 0) {
+
+
+		//printf("name: %s\n", seq->name.s);
+        //if (seq->comment.l)
+        //printf("comment: %s\n", seq->comment.s);
+		//printf("seq: %s\n", seq->seq.s);
+		//if (seq->qual.l) printf("qual: %s\n", seq->qual.s);
+        std::string name(seq->name.s);
+        std::string sequ(seq->seq.s);
+        std::string quali(seq->qual.s);
+        read_record v(name,sequ,quali);
+        allreads.push_back(v);
+	}
+    std::sort(allreads.begin(), allreads.end(), [&nameToRank](const read_record& r1,
+                const read_record& r2) -> bool {
+                //std::cout << r1.name << "\n";
+                //std::cout << r2.name << "\n";
+                //std::cout<< nameToRank[r1.name] << nameToRank[r2.name] << "\n" ;
+                return nameToRank[r1.name] < nameToRank[r2.name]; }) ;
+
+    std::ofstream ofs;
+    ofs.open(argv[3], std::ofstream::out) ;
+
+
+    for(auto &readid: allreads){
+        ofs << "@" << readid.name << "\n" ;
+        ofs << readid.seq<< "\n" ;
+        ofs << readid.comment << "\n" ;
+        ofs << readid.quality << "\n" ;
+    }
+
+	printf("return value: %d\n", l);
+	kseq_destroy(seq);
+    ifs.close() ;
+    ofs.close() ;
+	gzclose(fp);
+	return 0;
+}
