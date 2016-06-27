55c6bd87b87f7379ef69f182f1cc5c77c0bb46a7
Hirak <hsarkar@cs.stonybrook.edu>
2016-06-27 02:18:51 -0400

    1 Removed bugs added both pairs 2 Dependency on read reordering is removed 3.The size is managable
    TODO!
    Sailfish integration

--- src/encodeAsRefOnly.py
+++ src/encodeAsRefOnly.py
@@ -40,7 +40,7 @@ class island:
         return (self.start,self.end)
 
 
-def readEqClass(eqfile, fastaFile,fastqFile,oFile,threshold):
+def readEqClass(eqfile, fastaFile,f1,f2,oFile,threshold):
     with open(eqfile) as ifile:
         numTran = int(ifile.readline().rstrip())
         numEq = int(ifile.readline().rstrip())
@@ -70,17 +70,16 @@ def readEqClass(eqfile, fastaFile,fastqFile,oFile,threshold):
             tids = tuple(toks[1:int(nt)+1])
             count = toks[-1]
             readnames = tuple(toks[int(nt)+2:-1])
-            if len(readnames) > threshold:
-                encode(readnames,fastaFile,fastqFile,oFile,tnames)
-            elif len(readnames) <= threshold and len(readnames)>2:
-                encodeAsShift(readnames,fastqFile,oFile)
+            if len(readnames) > 20:
+                encode(readnames,fastaFile,f1,f2,oFile,tnames)
             else:
                 with open(oFile,'a') as wFile:
                     for rid in readnames:
                         headerno=int(rid.split(" ")[0].split("_")[1])
                         import linecache
-                        seq = linecache.getline(fastqFile,headerno*2).rstrip()
-                        wFile.write("{}\n".format(seq))
+                        seq = linecache.getline(f1,headerno*2).rstrip()
+                        seqm = linecache.getline(f2,headerno*2).rstrip()
+                        wFile.write("{}:{}\n".format(seq,seqm))
 
     print "\n"
 
@@ -187,8 +186,126 @@ def encodeAsShift(readnames,fastqFile,oFile):
             prevpos = currpos
             wFile.write("{}{}\n".format(wstr,fwd))
 
+def manageNeg(pos,seqr,tseq):
+    overhang = seqr[0:abs(pos)]
+    i = 0
+    while((abs(pos)+i < len(seqr))and(tseq[i] == seqr[abs(pos)+i])):
+        #print tseq[i],seqr[abs(pos)+i],i,abs(pos)+i,len(seqr)
+        i = i + 1
+    matches = i
+    if i < len(seqr):
+        leftstr = overhang+"M"+str(matches)+seqr[i:]
+        #wFile.write("{}M{}{}\n".format(overhang,matches,seqr[i:]))
+    else:
+        leftstr = overhang + "M" + str(matches)
 
-def encode(readnames,fastaFile,fastqFile,oFile,tname):
+    return leftstr
+
+
+def manageRight(pos,seqr,tseq):
+    i = 0
+    overhang = ''
+    match = 0
+    counter = 0
+    encoded = ''
+    while(pos+counter <= len(tseq)-1 and counter < len(seqr)):
+        #if tid == 8719:
+        #    print len(seqr),seqr,pos,len(tseq)
+        if(tseq[pos+counter] == seqr[counter]):
+            match += 1
+        else:
+            if(match < 3):
+                if match == 0:
+                    encoded += seqr[counter]
+                else:
+                    encoded += seqr[counter-match:match+1]
+            else:
+                encoded += "M" + str(match) + seqr[counter]
+            match = 0
+
+        counter += 1
+    if(match > 0):
+        encoded += "M"+str(match)
+    if(counter < len(seqr)):
+        encoded += seqr[counter:]
+
+    return encoded
+
+def manageLeft(pos,seqr,prevseq,currseq,prevpos,currpos,tseq):
+    i = 0
+    overhang = ''
+    match = 0
+    counter = 0
+    encoded = ''
+    wstr = ''
+    while(pos+counter <= len(tseq)-1 and counter < len(seqr)):
+        #if tid == 8719:
+        #    print len(seqr),seqr,pos,len(tseq)
+        if(tseq[pos+counter] == seqr[counter]):
+            match += 1
+        else:
+            if(match < 3):
+                if match == 0:
+                    encoded += seqr[counter]
+                else:
+                    encoded += seqr[counter-match:match+1]
+            else:
+                encoded += "M" + str(match) + seqr[counter]
+            match = 0
+
+        counter += 1
+    if(match > 0):
+        encoded += "M"+str(match)
+    if(counter < len(seqr)):
+        encoded += seqr[counter:]
+
+    score = len(encoded)
+    #in this stage we have match statistics
+    #also the overhang statistics
+    #we can either go with shit theory or
+    #we can just encode it using position
+    #with respect to transcript
+    if (match < len(seqr)):
+        if((score > (len(seqr)* 0.8)) and (prevseq != '')):
+            #we have to decide either to go with refbased
+            #encoding or shift encoding with respect to
+            #previous sequence
+            shift = currpos - prevpos
+            counter = 0
+            match = 0
+            encodeshift = False
+            if(shift > 0 and shift < len(prevseq)):
+                wstr += 'S'+str(shift)
+            else:
+                shift = 0
+
+            while((shift+counter <= len(prevseq)-1) and (counter < len(currseq))):
+                if(prevseq[shift+counter] == currseq[counter]):
+                    match = match + 1
+                else:
+                    if(match < 3):
+                        if match == 0:
+                            wstr += currseq[counter]
+                        else:
+                            wstr += currseq[counter - match : match+1]
+                    else:
+                        wstr += 'M'+str(match)+currseq[counter]
+                    match = 0
+                counter += 1
+            if(match > 0):
+                wstr += 'M'+str(match)
+        else:
+            wstr = encoded
+            #wstr = "M"+str(matches)+seqr[i:]
+            #wFile.write("M{}{}\n".format(matches,seqr[i:]))
+    else:
+        wstr = encoded
+        #wFile.write("M{}\n".format(matches))
+
+    return wstr
+
+
+def encode(readnames,fastaFile,f1,f2,oFile,tname):
     tid = int(readnames[0].split(" ")[3])
     import linecache
     tseq = linecache.getline(fastaFile,tid*2+2).rstrip()
@@ -196,16 +313,20 @@ def encode(readnames,fastaFile,fastqFile,oFile,tname):
     #print "tsq: ",tseq[:20]
     #sort the read names
     leftreads = []
+    rightreads = []
     for rid in readnames:
         headerno=int(rid.split(" ")[0].split("_")[1])
-        seq = linecache.getline(fastqFile,headerno*2).rstrip()
-        leftreads.append((rid.split(" ")[0],seq,int(rid.split(" ")[5]),int(rid.split(" ")[2])))
+        seq = linecache.getline(f1,headerno*2).rstrip()
+        seqR = linecache.getline(f2,headerno*2).rstrip()
+        leftreads.append((rid.split(" ")[0],seq,int(rid.split(" ")[5]),int(rid.split(" ")[2]), \
+            seqR,int(rid.split(" ")[4]),int(rid.split(" ")[1])))
+        #rightreads.append((rid.split(" ")[0],seqR,int(rid.split(" ")[4]),int(rid.split(" ")[1])))
         #print rid.split(" ")[0],int(rid.split(" ")[2])
 
     import operator
     sortedl = sorted(leftreads,key=operator.itemgetter(3))
     #discover islands
-    boundaries =[(h,p,p+len(s)) for h,s,_,p in sortedl]
+    boundaries =[(h,p,p+len(s)) for h,s,_,p,_,_,_ in sortedl]
     islands, islandDict  = discoverIsland(tseq,boundaries)
 
     minpos = min(leftreads,key=operator.itemgetter(3))[3]
@@ -237,14 +358,20 @@ def encode(readnames,fastaFile,fastqFile,oFile,tname):
 
     with open(oFile,'a') as wFile:
         #wFile.write("{}\t{}\t{}\n".format(tseq[minpos:(maxpos-minpos)+54],minpos,maxpos))
-        for header,seq,fwd,pos in sortedl:
+        for header,seq,fwd,pos,seqM,fwdM,posM in sortedl:
             #print header
             s,e = islands[islandDict[header]].get()
             relpos=0
+            relposM = 0
             if pos >= 0:
                 relpos = pos-s
             else:
                 relpos = pos
+
+            if posM >= 0:
+                relposM = posM-s
+            else:
+                relposM = posM
 
             currId = islandDict[header]
             if lastId == -1:
@@ -254,128 +381,92 @@ def encode(readnames,fastaFile,fastqFile,oFile,tname):
             elif lastId != currId:
                 wFile.write("{}\t{}\t".format(relpos,islandDict[header]))
 
-
             lastId = currId
+
+            lOrphan = False
+            rOrphan = False
+
             seqr = revcomp(seq) if(not(fwd)) else seq
+            seqm = revcomp(seqM) if(not(fwdM)) else seqM
+
+            #for left end
             overhang = ''
             currseq = seqr
             currpos = pos
             wstr = ''
+            #for right end
+            overhangM = ''
+            currseqM = seqm
+            currposM = posM
+            wstr = ''
             #print abs(pos)
             #if it is unmapped only hope is to encode like previous
-            if(pos == 0):
-                if(noRef):
-                    ref = seqr
-                    wFile.write("{}{}\n".format(seqr,fwd))
-                    noRef = False
-                else:
-                    i = 0
-                    while((i<len(seqr))and(ref[i] == seqr[i])):
-                        i = i+1
-                    if(i > len(seqr)-2):
-                        wFile.write("M{}{}\n".format(i,fwd))
+
+            #start working on the right end Now it becomes more complex
+            #because either of the ends might be unmapped handle both ends
+            #differently then concatenate them use flags to control them
+            #leftstr and rightstr will be touched once and once only
+
+            rightstr = ''
+            leftstr = ''
+
+
+            if(pos == 0 or posM == 0):
+                #get done with orphan read
+                if(pos == 0):
+                    lOrphan = True
+                    if(noRef):
+                        ref = seqr
+                        #wFile.write("{}{}".format(seqr,fwd))
+                        leftstr = "{}{}".format(seqr,fwd)
+                        noRef = False
                     else:
-                        wFile.write("{}{}\n".format(seqr,fwd))
-                        ref=seqr
-                continue
+                        i = 0
+                        while((i<len(seqr))and(ref[i] == seqr[i])):
+                            i = i+1
+                        if(i > len(seqr)-2):
+                            #wFile.write("M{}{}".format(i,fwd))
+                            leftstr="M{}{}".format(i,fwd)
+                        else:
+                            leftstr = "{}{}".format(seqr,fwd)
+                            ref=seqr
+                if(posM == 0):
+                    rOrphan = True
+                    rightstr = "{}".format(seqm)
 
             if(pos < 0):
-                overhang = seqr[0:abs(pos)]
-                i = 0
-                while((abs(pos)+i < len(seqr))and(tseq[i] == seqr[abs(pos)+i])):
-                    #print tseq[i],seqr[abs(pos)+i],i,abs(pos)+i,len(seqr)
-                    i = i + 1
-                matches = i
-                if i < len(seqr):
-                    wstr = overhang+"M"+str(matches)+seqr[i:]
-                    #wFile.write("{}M{}{}\n".format(overhang,matches,seqr[i:]))
-                else:
-                    wstr = overhang + "M" + str(matches)
-                    #wFile.write("{}M{}\n".format(overhang,matches))
-            else:
-                i = 0
-                overhang = ''
-                match = 0
-                counter = 0
-                encoded = ''
-                while(pos+counter <= len(tseq)-1 and counter < len(seqr)):
-                    #if tid == 8719:
-                    #    print len(seqr),seqr,pos,len(tseq)
-                    if(tseq[pos+counter] == seqr[counter]):
-                        match += 1
-                    else:
-                        if(match < 3):
-                            if match == 0:
-                                encoded += seqr[counter]
-                            else:
-                                encoded += seqr[counter-match:match+1]
-                        else:
-                            encoded += "M" + str(match) + seqr[counter]
-                        match = 0
+                leftstr = manageNeg(pos,seqr,tseq)
+            if(posM < 0):
+                rightstr = manageNeg(posM,seqm,tseq)
 
-                    counter += 1
-                if(match > 0):
-                    encoded += "M"+str(match)
-                if(counter < len(seqr)):
-                    encoded += seqr[counter:]
 
-                score = len(encoded)
-                #in this stage we have match statistics
-                #also the overhang statistics
-                #we can either go with shit theory or
-                #we can just encode it using position
-                #with respect to transcript
-                if (match < len(seqr)):
-                    if((score > (len(seqr)* 0.8)) and (prevseq != '')):
-                        #we have to decide either to go with refbased
-                        #encoding or shift encoding with respect to
-                        #previous sequence
-                        shift = currpos - prevpos
-                        counter = 0
-                        match = 0
-                        encodeshift = False
-                        if(shift > 0 and shift < len(prevseq)):
-                            wstr += 'S'+str(shift)
-                        else:
-                            shift = 0
-
-                        while((shift+counter <= len(prevseq)-1) and (counter < len(currseq))):
-                            if(prevseq[shift+counter] == currseq[counter]):
-                                match = match + 1
-                            else:
-                                if(match < 3):
-                                    if match == 0:
-                                        wstr += currseq[counter]
-                                    else:
-                                        wstr += currseq[counter - match : match+1]
-                                else:
-                                    wstr += 'M'+str(match)+currseq[counter]
-                                match = 0
-                            counter += 1
-                        if(match > 0):
-                            wstr += 'M'+str(match)
-                    else:
-                        wstr = encoded
-                        #wstr = "M"+str(matches)+seqr[i:]
-                        #wFile.write("M{}{}\n".format(matches,seqr[i:]))
-                else:
-                    wstr = encoded
-                    #wFile.write("M{}\n".format(matches))
-
+            if(pos > 0):
                 #logic for writing L which blindly
                 #encode the previous line
+                #call manageLeft(pos,seqr,prevseq,currseq,tseq):
+                leftstr = manageLeft(pos,seqr,prevseq,currseq,prevpos,currpos,tseq)
                 if prev == '':
-                    prev = wstr
-                    wFile.write("{}{}\n".format(wstr,fwd))
+                    prev = leftstr
+                    leftstr = leftstr+str(fwd)
+                    #wFile.write("{}{}\n".format(wstr,fwd))
                 else:
-                    if wstr == prev:
-                        wFile.write("{}\n".format(fwd))
+                    if leftstr == prev:
+                        #wFile.write("{}\n".format(fwd))
+                        leftstr = str(fwd)
                     else:
-                        wFile.write("{}{}\n".format(wstr,fwd))
-                        prev = wstr
-
+                        #wFile.write("{}{}\n".format(wstr,fwd))
+                        leftstr = leftstr+str(fwd)
+                        prev = leftstr
                 prevseq = currseq
                 prevpos = currpos
+
+            if(posM > 0):
+                rightstr = manageRight(posM,seqm,tseq)
+
+            wFile.write("{}:{}\t{}{}\n".format(leftstr,relposM,rightstr,fwdM))
+
+
+
 
 
 
@@ -384,16 +475,18 @@ def main():
     parser = argparse.ArgumentParser(description="blah blah")
     parser.add_argument('-i',type=str,help="eq class file")
     parser.add_argument('-tr',type=str,help="transcript file")
-    parser.add_argument('-fq',type=str,help="fastq file")
+    parser.add_argument('-f1',type=str,help="fastq file")
+    parser.add_argument('-f2',type=str,help="fastq file")
     parser.add_argument('-t',type=int,help="threshold value")
     parser.add_argument('-o',type=str,help="output file")
     args = parser.parse_args()
     eqfile = args.i
     fastaFile = args.tr
-    fastqFile = args.fq
+    f1 = args.f1
+    f2 = args.f2
     oFile = args.o
     threshold = args.t
-    readEqClass(eqfile,fastaFile,fastqFile,oFile,threshold)
+    readEqClass(eqfile,fastaFile,f1,f2,oFile,threshold)
 
 
 if __name__ == "__main__":
